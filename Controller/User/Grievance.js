a4c46ba15b47e4da8d8fda1595250ffe400bbd2b
Anirban Karmakar <karmakar_anirban89@outlook.com>
2021-11-25 19:07:15 +0530

    Send response function change in api

--- Controller/User/Grievance.js
+++ Controller/User/Grievance.js
@@ -4,14 +4,14 @@ const { Validator } = require('node-input-validator');
 const COMPLAINT_MODEL = require('../../Models/grievance');
 var Upload = require('../../service/upload');
 
-var addGrievance = async (req,res)=>{
+var addGrievance = async (req, res) => {
     const V = new Validator(req.body, {
         name: 'required',
         email: 'required|email',
         report_against: 'required',
         report_detail: 'required'
     });
-    let matched = V.check().then(val=>val);
+    let matched = V.check().then(val => val);
 
     if (!matched) {
         return res.status(400).json({ status: false, errors: V.errors });
@@ -26,23 +26,23 @@ var addGrievance = async (req,res)=>{
         report_detail: req.body.report_detail
     }
     if (
-        req.body.phone != '' || 
-        req.body.phone != null || 
+        req.body.phone != '' ||
+        req.body.phone != null ||
         typeof req.body.phone != "undefined"
     ) {
         complaintData.phone = req.body.phone;
     }
     if (
-        req.body.url != '' || 
-        req.body.url != null || 
+        req.body.url != '' ||
+        req.body.url != null ||
         typeof req.body.url != "undefined"
     ) {
         complaintData.url = req.body.url;
     }
     let attachment_url = await Upload.uploadFile(req, "grievances");
     if (
-        req.file != '' || 
-        req.file != null || 
+        req.file != '' ||
+        req.file != null ||
         typeof req.file != "undefined"
     ) {
         complaintData.attachment = attachment_url;
@@ -50,22 +50,21 @@ var addGrievance = async (req,res)=>{
 
     const NEW_COMPLAINT = new COMPLAINT_MODEL(complaintData);
 
-    return NEW_COMPLAINT.save((err,docs)=>{
-        if (!err) {
+    return NEW_COMPLAINT.save()
+        .then(docs => {
             res.status(200).json({
                 status: true,
                 message: "Complaint registered successfully.",
                 data: docs
             });
-        }
-        else {
+        })
+        .catch(err => {
             res.status(500).json({
                 status: false,
                 message: "Failed to register complaint. Server error.",
                 error: err
             });
-        }
-    });
+        });
 }
 
 module.exports = {
