b67b4aea45d03c7aaeebd713c56290fbc888711d
Anirban Karmakar <karmakar_anirban89@outlook.com>
2022-03-21 18:02:07 +0530

    Admin add article

---
+++ Models/Website_info/articles.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var moment = require('moment-timezone');
+
+var Schema = mongoose.Schema;
+var dateKolkata = moment.tz(Date.now(), "Asia/Kolkata");
+
+const ARTICLE_SCHEMA = new Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: {
+        type: String,
+        required: true
+    },
+    details: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    author: String,
+    dated: {
+        type: Date,
+        default: dateKolkata
+    }
+});
+
+module.exports = mongoose.model("article", ARTICLE_SCHEMA);
