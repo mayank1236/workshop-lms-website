6ff067e507ff9129f180b30035c8a79501f15b20
ananya1788 <pal.ananya88@gmail.com>
2022-07-20 11:34:08 +0530

    user details added in viewShopServiceDetails

--- Controller/User/ShopServices.js
+++ Controller/User/ShopServices.js
@@ -416,14 +416,18 @@ const viewShopServiceDetails = async (req, res) => {
                             as: "seller_details"
                         }
                     },
+                   
                     {
                         $lookup: {
                             from: "service_reviews",
                             localField: "_id",
-                            foreignField: "service_id",
+                            foreignField: "service_id",                       
+                           
                             as: "review_data"
                         }
-                    },
+                    },                
+              
+                     
                     {
                         $addFields: {
                             avgRating: {
@@ -436,6 +440,39 @@ const viewShopServiceDetails = async (req, res) => {
                             }
                         }
                     },
+                    {
+                        $unwind: {
+                            path: "$review_data",
+                            preserveNullAndEmptyArrays: true
+                          }
+                    },
+                
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "review_data.user_id",
+                            foreignField: "_id",                       
+                        //     let: { userid: "$review_data.user_id",
+                        //  },
+                        //     pipeline: [{
+                                
+                        //         $match: {
+
+                        //             $expr: {
+                        //                 $and: [{ $eq: ["$_id", "$$userid"] },
+                                       
+                        //                 ]
+                        //             }
+                        //         }
+                        //     }],
+                            as: "review_data.user_data"
+                        }
+                          
+                    },
+                 
+            
+
+                  
                     { $project: { _v: 0 } }
                 ]
             )
