6b58c720153523d59236a2b5017d8d04dfe5d7db
ananya1788 <pal.ananya88@gmail.com>
2022-07-23 16:32:31 +0530

    changes made in coupon api

--- Controller/User/Coupons.js
+++ Controller/User/Coupons.js
@@ -3,16 +3,21 @@ var mongoose = require("mongoose");
 const SERVICE_COUPON = require("../../Models/coupon");
 const SERVICE_CART = require("../../Models/service_cart");
 const SERVICE_CHECKOUT = require("../../Models/checkout");
+const Curvalue = require("../../Models/currvalue");
 
 var applyCoupon = async (req, res) => {
     var user_id = req.body.user_id;
     var coup_name = req.body.coup_name;
+   // console.log(req.user);
+
+
 
     let coupData = await SERVICE_COUPON.findOne({
-        name: coup_name,
+        name: req.body.coup_name,
         status: true,
     }).exec();
-
+   console.log("coupData"+coupData);
+    
     if (coupData.times == 0) {
         return res.status(500).json({
             status: false,
@@ -48,8 +53,27 @@ var applyCoupon = async (req, res) => {
                 }
             ).exec();
 
-            // coupData.times -= 1;
-            // let availableCoupNum = await coupData.save();
+          if(req.user.currency!="CAD")
+         {
+
+           let conVert = await Curvalue.find({from:"CAD",to:req.user.currency}).exec();
+          let cal = coupData.minprice* conVert[0].value
+           coupData.minprice= cal.toFixed(2)
+
+           if(coupData.discount_type=='Flat discount'){
+
+            // let conVert = await Curvalue.find({from:"CAD",to:req.user.currency}).exec();
+            let cal = coupData.discount_value* conVert[0].value
+             coupData.discount_value= cal.toFixed(2)
+
+        }
+
+
+         }
+
+       
+
+     
 
             return res.status(200).json({
                 status: true,
