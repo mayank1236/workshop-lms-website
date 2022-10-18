dda6b76c6f03f9973be2036f6aced5354c8a4dd4
nirmal <nir.multi2018@gmail.com>
2022-10-18 18:00:06 +0530

    up

--- Controller/User/Myaccount.js
+++ Controller/User/Myaccount.js
@@ -117,7 +117,8 @@ var viewAll = async (req, res) => {
     {
       $group: {
         _id: "$order_id",
-        order_subtotal: { $sum: "$cart_data.price" },
+        // order_subtotal: { $sum: "$cart_data.price" },
+        order_subtotal: "$total",
         discount: { $sum: "$cart_data.discount_percent" },
         user_data: { $push: "$user_data" },
         cart_data: { $push: "$cart_data" },
