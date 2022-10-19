6c27cba423a71b78fc9a2ba860006ef3d4a568e2
nirmal <nir.multi2018@gmail.com>
2022-10-19 16:49:41 +0530

    up

--- Controller/Auth/Admin.js
+++ Controller/Auth/Admin.js
@@ -1,306 +1,304 @@
-var mongoose = require('mongoose')
-var Admin = require('../../Models/admin')
-var passwordHash = require('password-hash');
-const Upload = require('../../service/upload');
-const BlogType = require('../../Models/blogType');
+var mongoose = require("mongoose");
+var Admin = require("../../Models/admin");
+var passwordHash = require("password-hash");
+const Upload = require("../../service/upload");
+const BlogType = require("../../Models/blogType");
 // const { Validator } = require('node-input-validator');
 // var AdminDetails=require('../../Models/admin_details');
+var Curvalue = require("../../Models/currvalue");
 
-var jwt = require('jsonwebtoken');
-const { Validator } = require('node-input-validator');
+var jwt = require("jsonwebtoken");
+const { Validator } = require("node-input-validator");
 
 function createToken(data) {
-    return jwt.sign(data, 'DonateSmile');
+  return jwt.sign(data, "DonateSmile");
 }
 
 const getTokenData = async (token) => {
-    let adminData = await Admin.findOne({ token: token }).exec();
-    // console.log('adminData', adminData);
-    return adminData;
-}
+  let adminData = await Admin.findOne({ token: token }).exec();
+  // console.log('adminData', adminData);
+  return adminData;
+};
 
-const register = async(req,res)=>{
-    const v = new Validator(req.body,{
-        email:'required|email',
-        password:'required',
-        fullname:'required'
+const register = async (req, res) => {
+  const v = new Validator(req.body, {
+    email: "required|email",
+    password: "required",
+    fullname: "required",
+  });
+  let matched = await v.check().then((val) => val);
+  if (!matched) {
+    return res.status(200).send({ status: false, error: v.errors });
+  }
+  let adminData = {
+    _id: mongoose.Types.ObjectId(),
+    email: req.body.email,
+    password: passwordHash.generate(req.body.password),
+    token: createToken(req.body),
+  };
+  if (typeof req.body.phone != "undefined") {
+    adminData.phone = Number(req.body.phone);
+  }
+
+  const admin = new Admin(adminData);
+
+  return admin
+    .save()
+    .then((data) => {
+      res.status(200).json({
+        status: true,
+        success: true,
+        message: "New Admin created successfully",
+        data: data,
+      });
     })
-    let matched = await v.check().then((val)=>val)
-    if(!matched)
-    {
-        return res.status(200).send({ status: false, error: v.errors });
-    }
-    let adminData = {
-        _id:mongoose.Types.ObjectId(),
-        email:req.body.email,
-        password:passwordHash.generate(req.body.password),
-        token:createToken(req.body)
-    }
-    if (typeof (req.body.phone) !='undefined')
-    {
-        adminData.phone = Number(req.body.phone)
-    }
+    .catch((error) => {
+      res.status(200).json({
+        status: false,
+        success: false,
+        message: "Server error. Please try again.",
+        error: error,
+      });
+    });
+};
 
-    const admin = new Admin(adminData)
+const login = async (req, res) => {
+  let v = new Validator(req.body, {
+    email: "required|email",
+    password: "required",
+  });
+  let matched = await v.check().then((val) => val);
+  if (!matched) {
+    return res.status(401).json({
+      status: false,
+      error: v.errors,
+    });
+  }
 
-    return admin.save().then((data)=>{
-        res.status(200).json({
-            status: true,
-            success: true,
-            message: 'New Admin created successfully',
-            data: data,
-        })
-    })
-        .catch((error)=>{
-            res.status(200).json({
-                status: false,
-                success: false,
-                message: 'Server error. Please try again.',
-                error: error,
-            });
-        })
-}
-
-const login = async(req,res) =>
-{
-    let v = new Validator(req.body,{
-        email:'required|email',
-        password:'required'
-    })
-    let matched = await v.check().then((val)=>val)
-    if(!matched)
-    {
+  // return Admin.findOne({email : req.body.email}, async(err,admin)=>{
+  //     console.log(admin)
+  //     if(err)
+  //     {
+  //         res.status(200).json({
+  //             status: false,
+  //             message: 'Server error. Please try again.',
+  //             error: err,
+  //         });
+  //     }
+  //     else if(admin!='' && admin.comparePassword(req.body.password))
+  //     {
+  //         res.status(200).json({
+  //             status: true,
+  //             message: 'Admin login successful',
+  //             data: admin
+  //         });
+  //     }
+  //     else
+  //     {
+  //         res.status(200).json({
+  //             status: false,
+  //             message: 'Server error. Please try again.',
+  //             error: err,
+  //         });
+  //     }
+  // })
+  Admin.findOne({ email: req.body.email })
+    //   .exe()
+    .then((admin) => {
+      if (admin != null && admin != "" && admin.length < 1) {
         return res.status(401).json({
-            status:false,
-            error: v.errors
-        })
-    }
-
-    // return Admin.findOne({email : req.body.email}, async(err,admin)=>{
-    //     console.log(admin)
-    //     if(err)
-    //     {
-    //         res.status(200).json({
-    //             status: false,
-    //             message: 'Server error. Please try again.',
-    //             error: err,
-    //         });
-    //     }
-    //     else if(admin!='' && admin.comparePassword(req.body.password))
-    //     {
-    //         res.status(200).json({
-    //             status: true,
-    //             message: 'Admin login successful',
-    //             data: admin
-    //         });
-    //     }
-    //     else
-    //     {
-    //         res.status(200).json({
-    //             status: false,
-    //             message: 'Server error. Please try again.',
-    //             error: err,
-    //         });
-    //     }
-    // })
-    Admin.findOne({email:req.body.email})
-        //   .exe()
-        .then(admin =>{
-            if(admin!=null && admin!='' && admin.length < 1 ) 
-            {
-                return res.status(401).json({
-                    status: false,
-                    message: 'Server error. Please try again.',
-                    error: 'Server Error',
-                });
-            }
-            if(admin!=null && admin!='' && admin.comparePassword(req.body.password)) 
-            {
-                return res.status(200).json({
-                    status: true,
-                    message: 'Admin login successful',
-                    data: admin
-                });
-            }
-            else
-            {
-                return res.status(200).json({
-                    status: false,
-                    message: 'Server error. Please try again.',
-                    error: 'Server Error',
-                });
-            }
-        }
-
-        )
-}
+          status: false,
+          message: "Server error. Please try again.",
+          error: "Server Error",
+        });
+      }
+      if (
+        admin != null &&
+        admin != "" &&
+        admin.comparePassword(req.body.password)
+      ) {
+        return res.status(200).json({
+          status: true,
+          message: "Admin login successful",
+          data: admin,
+        });
+      } else {
+        return res.status(200).json({
+          status: false,
+          message: "Server error. Please try again.",
+          error: "Server Error",
+        });
+      }
+    });
+};
 
 var uploadImage = async (req, res) => {
-    let imageUrl = "";
-    let image_url = await Upload.uploadFile(req, 'admin')
-    if (req.file != null && req.file != '' && typeof (req.file) != 'undefined') {
-        imageUrl = image_url;
-    }
+  let imageUrl = "";
+  let image_url = await Upload.uploadFile(req, "admin");
+  if (req.file != null && req.file != "" && typeof req.file != "undefined") {
+    imageUrl = image_url;
+  }
 
-    res.status(200).json({
-        status: true,
-        data: imageUrl,
-        error: null
-    })
-
-}
+  res.status(200).json({
+    status: true,
+    data: imageUrl,
+    error: null,
+  });
+};
 var update = async (req, res) => {
-
-    Admin.findOneAndUpdate(
-        { _id: { $in: [mongoose.Types.ObjectId(req.params.id)] } },
-        req.body,
-        { new: true },
-        (err, data) => {
-            if (err) {
-                res.status(500).json({
-                    status: false,
-                    message: "Invalid id. Server error.",
-                    error: err
-                });
-            }
-            else {
-                res.status(200).json({
-                    status: true,
-                    message: "Updated admin data successfully",
-                    data: data
-                });
-            }
-        }
-    )
-
-}
+  Admin.findOneAndUpdate(
+    { _id: { $in: [mongoose.Types.ObjectId(req.params.id)] } },
+    req.body,
+    { new: true },
+    (err, data) => {
+      if (err) {
+        res.status(500).json({
+          status: false,
+          message: "Invalid id. Server error.",
+          error: err,
+        });
+      } else {
+        res.status(200).json({
+          status: true,
+          message: "Updated admin data successfully",
+          data: data,
+        });
+      }
+    }
+  );
+};
 
 var viewAll = async (req, res) => {
-    let adminProfile = await Admin.find({
-        status: true,
-        admin_type: "Admin"
-    }).exec();
+  let adminProfile = await Admin.find({
+    status: true,
+    admin_type: "Admin",
+  }).exec();
 
-    if (adminProfile.length > 0) {
-        res.status(200).json({
-            status: true,
-            message: "Data get successfully ",
-            data: adminProfile,
-
-        })
-    }
-    else {
-        return res.status(200).json({
-            status: true,
-            message: "Currently no active data exists",
-            data: ''
-        });
-    }
-}
+  if (adminProfile.length > 0) {
+    res.status(200).json({
+      status: true,
+      message: "Data get successfully ",
+      data: adminProfile,
+    });
+  } else {
+    return res.status(200).json({
+      status: true,
+      message: "Currently no active data exists",
+      data: "",
+    });
+  }
+};
 
 var addBlogData = async (req, res) => {
-    console.log(req.body);
-    let blogData = {
-        _id: mongoose.Types.ObjectId(),
-        blog_type: req.body.blog_type
-    }
+  console.log(req.body);
+  let blogData = {
+    _id: mongoose.Types.ObjectId(),
+    blog_type: req.body.blog_type,
+  };
 
-    const new_blog = new BlogType(blogData)
-    return new_blog.save((err, data) => {
-        if (!err) {
-            res.status(200).json({
-                status: true,
-                message: "New blog added successfully!",
-                data: data
-            });
-        }
-        else {
-            res.status(500).json({
-                status: false,
-                message: "Failed to add blog. Server error.",
-                error: err
-            });
-        }
-    })
-}
+  const new_blog = new BlogType(blogData);
+  return new_blog.save((err, data) => {
+    if (!err) {
+      res.status(200).json({
+        status: true,
+        message: "New blog added successfully!",
+        data: data,
+      });
+    } else {
+      res.status(500).json({
+        status: false,
+        message: "Failed to add blog. Server error.",
+        error: err,
+      });
+    }
+  });
+};
 
 const viewBlogData = async (req, res) => {
-    let blogs = await BlogType.find().exec();
+  let blogs = await BlogType.find().exec();
 
-    if (blogs.length > 0) {
-        return res.status(200).json({
-            status: true,
-            message: "All blogs successfully get.",
-            data: blogs
-        });
-    }
-    else {
-        return res.status(200).json({
-            status: true,
-            message: "No information to show.",
-            data: null
-        });
-    }
-
-}
+  if (blogs.length > 0) {
+    return res.status(200).json({
+      status: true,
+      message: "All blogs successfully get.",
+      data: blogs,
+    });
+  } else {
+    return res.status(200).json({
+      status: true,
+      message: "No information to show.",
+      data: null,
+    });
+  }
+};
 
 const editBlogData = async (req, res) => {
-    return BlogType.findOneAndUpdate(
-        { _id: mongoose.Types.ObjectId(req.params.id) },
-        req.body,
-        { new: true },
-        (err, data) => {
-            if (!err) {
-                res.status(200).json({
-                    status: true,
-                    message: "Blog successfully edited.",
-                    data: data
-                });
-            }
-            else {
-                res.status(500).json({
-                    status: false,
-                    message: "Invalid id.",
-                    error: err
-                });
-            }
-
-
-        })
-}
+  return BlogType.findOneAndUpdate(
+    { _id: mongoose.Types.ObjectId(req.params.id) },
+    req.body,
+    { new: true },
+    (err, data) => {
+      if (!err) {
+        res.status(200).json({
+          status: true,
+          message: "Blog successfully edited.",
+          data: data,
+        });
+      } else {
+        res.status(500).json({
+          status: false,
+          message: "Invalid id.",
+          error: err,
+        });
+      }
+    }
+  );
+};
 
 const deleteBlogData = async (req, res) => {
-    return BlogType.findByIdAndDelete(
-        { _id: mongoose.Types.ObjectId(req.params.id) },
-        (err, data) => {
+  return BlogType.findByIdAndDelete(
+    { _id: mongoose.Types.ObjectId(req.params.id) },
+    (err, data) => {
+      if (!err) {
+        res.status(200).json({
+          status: true,
+          message: "Blog deleted successfully.",
+          data: data,
+        });
+      } else {
+        res.status(500).json({
+          status: false,
+          message: "Invalid id.",
+          error: err,
+        });
+      }
+    }
+  );
+};
 
-            if (!err) {
-                res.status(200).json({
-                    status: true,
-                    message: "Blog deleted successfully.",
-                    data: data
-                });
-            }
-            else {
-                res.status(500).json({
-                    status: false,
-                    message: "Invalid id.",
-                    error: err
-                });
-            }
+const currencyVal = async (req, res) => {
+  let conVert = await Curvalue.find({
+    from: req.body.from,
+    to: req.body.to,
+  }).exec();
 
-        })
-}
+  res.status(200).json({
+    status: true,
+    data: conVert[0].value,
+    error: null,
+  });
+};
 
 module.exports = {
-    register,
-    getTokenData,
-    login,
-    update,
-    uploadImage,
-    viewAll,
-    addBlogData,
-    viewBlogData,
-    editBlogData,
-    deleteBlogData
-}
+  register,
+  getTokenData,
+  login,
+  update,
+  uploadImage,
+  viewAll,
+  addBlogData,
+  viewBlogData,
+  editBlogData,
+  deleteBlogData,
+  currencyVal,
+};
--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,128 +1,137 @@
-const express = require('express');
-const multer = require('multer');
-var nodeCron = require('node-cron');
-
+const express = require("express");
+const multer = require("multer");
+var nodeCron = require("node-cron");
 
 const router = express.Router();
 
 var storage = multer.memoryStorage();
-var upload = multer({storage: storage});
+var upload = multer({ storage: storage });
 
-const AdminController = require('../../Controller/Auth/Admin');
-const UserController = require('../../Controller/Auth/User');
-const ProductController = require('../../Controller/User/Product');// added by anirbank-93
-const ServiceController = require('../../Controller/User/ServiceCategory');// added by anirbank-93
-const UserSellersController = require('../../Controller/User/UserSellers');// added by anirbank-93
-const ShopServiceController = require('../../Controller/User/ShopServices');// added by anirbank-93
-const SearchController = require('../../Controller/User/Search');  // added by anirbank-93
-const UserContact = require('../../Controller/User/UserContact');  // added by anirbank-93
-const BlogController = require('../../Controller/User/Blog');
-const BlogComments = require('../../Controller/User/BlogComments');
+const AdminController = require("../../Controller/Auth/Admin");
+const UserController = require("../../Controller/Auth/User");
+const ProductController = require("../../Controller/User/Product"); // added by anirbank-93
+const ServiceController = require("../../Controller/User/ServiceCategory"); // added by anirbank-93
+const UserSellersController = require("../../Controller/User/UserSellers"); // added by anirbank-93
+const ShopServiceController = require("../../Controller/User/ShopServices"); // added by anirbank-93
+const SearchController = require("../../Controller/User/Search"); // added by anirbank-93
+const UserContact = require("../../Controller/User/UserContact"); // added by anirbank-93
+const BlogController = require("../../Controller/User/Blog");
+const BlogComments = require("../../Controller/User/BlogComments");
 /**-------------------------Website info section------------------------- */
-const AboutUs = require('../../Controller/User/Website_info/AboutUs');
-const TermsNConditn = require('../../Controller/User/Website_info/TermsNConditn');
-const PrivacyPolicy = require('../../Controller/User/Website_info/PrivacyPolicy');
-const SocialMediaInfo = require('../../Controller/User/Website_info/SocialMediaInfo');
-const ContactUsInfo = require('../../Controller/User/Website_info/ContactUsInfo');
-const SafetyGuide = require('../../Controller/User/Website_info/SafetyGuide');
-const Associates = require('../../Controller/User/Website_info/Associates');
-const LegalNotice = require('../../Controller/User/Website_info/LegalNotice');
-const Careers = require('../../Controller/User/Website_info/Careers');
-const CmsController = require('../../Controller/User/Website_info/Cms');
+const AboutUs = require("../../Controller/User/Website_info/AboutUs");
+const TermsNConditn = require("../../Controller/User/Website_info/TermsNConditn");
+const PrivacyPolicy = require("../../Controller/User/Website_info/PrivacyPolicy");
+const SocialMediaInfo = require("../../Controller/User/Website_info/SocialMediaInfo");
+const ContactUsInfo = require("../../Controller/User/Website_info/ContactUsInfo");
+const SafetyGuide = require("../../Controller/User/Website_info/SafetyGuide");
+const Associates = require("../../Controller/User/Website_info/Associates");
+const LegalNotice = require("../../Controller/User/Website_info/LegalNotice");
+const Careers = require("../../Controller/User/Website_info/Careers");
+const CmsController = require("../../Controller/User/Website_info/Cms");
 /**-----------------------Website info section end----------------------- */
 
-const middleware  = require('../../service/middleware').middleware;
+const middleware = require("../../service/middleware").middleware;
 
-const AdminRoute = require('./admin');
-const UserRoute = require('./user');
+const AdminRoute = require("./admin");
+const UserRoute = require("./user");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({status: false})
+router.get("/", function (req, res, next) {
+  res.send({ status: false });
 });
 
 /** ================================= without login url ================================= */
 
-router.post('/admin/register', AdminController.register);
-router.post('/admin/login', AdminController.login);
+router.post("/admin/register", AdminController.register);
+router.post("/admin/login", AdminController.login);
 
-router.post('/user/register', UserController.register); // add upload image option in future
-router.post('/user/login', UserController.login);
+router.post("/admin/curValue", AdminController.currencyVal);
 
-router.get('/user/listProducts', UserController.viewProductList);
-router.get('/user/viewproduct/:id', ProductController.viewSingleProduct);   // added by anirbank-93
+router.post("/user/register", UserController.register); // add upload image option in future
+router.post("/user/login", UserController.login);
 
-router.get('/user/service-category', ServiceController.viewAllServices);// added by anirbank-93
-router.get('/user/service-category/:id', ServiceController.viewService);// added by anirbank-93
-router.get('/user/rates', ProductController.rates);
+router.get("/user/listProducts", UserController.viewProductList);
+router.get("/user/viewproduct/:id", ProductController.viewSingleProduct); // added by anirbank-93
 
+router.get("/user/service-category", ServiceController.viewAllServices); // added by anirbank-93
+router.get("/user/service-category/:id", ServiceController.viewService); // added by anirbank-93
+router.get("/user/rates", ProductController.rates);
 
-router.get('/user/service-category/shop-services/:user_id/:id/:page', ServiceController.viewShopServicesPerService);// added by anirbank-93
+router.get(
+  "/user/service-category/shop-services/:user_id/:id/:page",
+  ServiceController.viewShopServicesPerService
+); // added by anirbank-93
 
-router.get('/user/shop/top-services', ShopServiceController.viewTopServiceProvider);// added by anirbank-93
+router.get(
+  "/user/shop/top-services",
+  ShopServiceController.viewTopServiceProvider
+); // added by anirbank-93
 
-router.get('/user/top-sellers', UserSellersController.viewTopSellers);  // added by anirbank-93
+router.get("/user/top-sellers", UserSellersController.viewTopSellers); // added by anirbank-93
 
-router.post('/user/search-service', SearchController.allSearch);   // added by anirbank-93
+router.post("/user/search-service", SearchController.allSearch); // added by anirbank-93
 
-router.post('/user/contact-us', upload.single("file"), UserContact.makeContact);
+router.post("/user/contact-us", upload.single("file"), UserContact.makeContact);
 
-router.get('/user/about-us', AboutUs.viewAllSegments);
-router.get('/user/about-us/:id', AboutUs.viewSegmentById);
+router.get("/user/about-us", AboutUs.viewAllSegments);
+router.get("/user/about-us/:id", AboutUs.viewSegmentById);
 
-router.get('/user/terms-and-condition', TermsNConditn.viewAllSegments);
-router.get('/user/terms-and-condition/:id', TermsNConditn.viewSegmentById);
+router.get("/user/terms-and-condition", TermsNConditn.viewAllSegments);
+router.get("/user/terms-and-condition/:id", TermsNConditn.viewSegmentById);
 
-router.get('/user/privacy-policy', PrivacyPolicy.viewAllSegments);
-router.get('/user/privacy-policy/:id', PrivacyPolicy.viewSegmentById);
+router.get("/user/privacy-policy", PrivacyPolicy.viewAllSegments);
+router.get("/user/privacy-policy/:id", PrivacyPolicy.viewSegmentById);
 
-router.get('/user/social-media-info', SocialMediaInfo.viewAll);
-router.get('/user/social-media-info/:id', SocialMediaInfo.viewById);
+router.get("/user/social-media-info", SocialMediaInfo.viewAll);
+router.get("/user/social-media-info/:id", SocialMediaInfo.viewById);
 
-router.get('/user/contact-us-info', ContactUsInfo.viewAll);
-router.get('/user/contact-us-info/:id', ContactUsInfo.viewById);
+router.get("/user/contact-us-info", ContactUsInfo.viewAll);
+router.get("/user/contact-us-info/:id", ContactUsInfo.viewById);
 
-router.get('/user/blog', BlogController.viewAllBlogs);
-router.get('/user/blog/:id', BlogController.viewBlogById);
+router.get("/user/blog", BlogController.viewAllBlogs);
+router.get("/user/blog/:id", BlogController.viewBlogById);
 
-router.get('/user/blog-comment', BlogComments.getAllComments);
+router.get("/user/blog-comment", BlogComments.getAllComments);
 
-router.get('/user/safety-guide', SafetyGuide.viewAllSegments);
-router.get('/user/safety-guide/:id', SafetyGuide.viewSegmentById);
+router.get("/user/safety-guide", SafetyGuide.viewAllSegments);
+router.get("/user/safety-guide/:id", SafetyGuide.viewSegmentById);
 
-router.get('/user/associate', Associates.viewAllAssociates);
-router.get('/user/associate/:id', Associates.viewAssociateById);
+router.get("/user/associate", Associates.viewAllAssociates);
+router.get("/user/associate/:id", Associates.viewAssociateById);
 
-router.get('/user/legal-notice', LegalNotice.viewAllSegments);
-router.get('/user/legal-notice/:id', LegalNotice.viewSegmentById);
+router.get("/user/legal-notice", LegalNotice.viewAllSegments);
+router.get("/user/legal-notice/:id", LegalNotice.viewSegmentById);
 
-router.get('/user/careers', Careers.viewAllPostedJobs);
-router.get('/user/careers/:id', Careers.viewPostedJobById);
-router.post('/user/apply-to-job', upload.single("cv"), Careers.applyToJob);
+router.get("/user/careers", Careers.viewAllPostedJobs);
+router.get("/user/careers/:id", Careers.viewPostedJobById);
+router.post("/user/apply-to-job", upload.single("cv"), Careers.applyToJob);
 
-router.get('/user/faq', CmsController.viewAllFAQs);
-router.get('/user/faq/:id', CmsController.viewFAQById);
-router.get('/user/articles', CmsController.viewAllArticles);
-router.get('/user/articles/:id', CmsController.viewArticleById);
-router.get('/user/cms/testimonials', CmsController.viewAllTestimonials);
-router.get('/user/cms/testimonials/:id', CmsController.viewTestimonialById);
+router.get("/user/faq", CmsController.viewAllFAQs);
+router.get("/user/faq/:id", CmsController.viewFAQById);
+router.get("/user/articles", CmsController.viewAllArticles);
+router.get("/user/articles/:id", CmsController.viewArticleById);
+router.get("/user/cms/testimonials", CmsController.viewAllTestimonials);
+router.get("/user/cms/testimonials/:id", CmsController.viewTestimonialById);
 
-router.get('/user/shop/shopservice-details/:id', ShopServiceController.viewShopServiceDetails); // added by anirbank-93
-router.get('/user/shop/shopservice-details-new/:id', ShopServiceController.viewShopServiceDet);
+router.get(
+  "/user/shop/shopservice-details/:id",
+  ShopServiceController.viewShopServiceDetails
+); // added by anirbank-93
+router.get(
+  "/user/shop/shopservice-details-new/:id",
+  ShopServiceController.viewShopServiceDet
+);
 
-router.get('/user/bannercontent', BlogComments.viewAllBAnner);
-router.get('/user/bannerAbout', BlogComments.viewAllaboutBAnner);
+router.get("/user/bannercontent", BlogComments.viewAllBAnner);
+router.get("/user/bannerAbout", BlogComments.viewAllaboutBAnner);
 
-nodeCron.schedule('0 0 * * *', ProductController.rates);
-
-
-
+nodeCron.schedule("0 0 * * *", ProductController.rates);
 
 /** ================================= without login url section end ================================ */
 
 router.use(middleware);
 
-router.use('/admin', AdminRoute);
-router.use('/user', UserRoute);
+router.use("/admin", AdminRoute);
+router.use("/user", UserRoute);
 
 module.exports = router;
