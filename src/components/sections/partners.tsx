e9973aa1255d3454bad79bdbe229cd10cf782907
Mayank Gupta <mayankgupta14042000@gmail.com>
2023-05-20 16:13:03 +0530

    partners section

--- src/components/sections/partners.tsx
+++ src/components/sections/partners.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import Section from '../Layout/Section'
+import Image from 'next/image'
+import style from '@/styles/partners.module.scss'
+import Container from '../Layout/Container'
 
 const Partners = () => {
     return (
-        <div>Partners</div>
+        <Section id="partners">
+            <Container>
+                <h2>Partners/Collaborators</h2>
+                <div className={style.partners}>
+                    <div>
+                        <Image src="/logo.png" fill={true} alt={'brand 1 logo'} />
+                    </div>
+                    <div>
+                        <Image src="/logo.png" fill={true} alt={'brand 1 logo'} />
+                    </div>
+                    <div>
+                        <Image src="/logo.png" fill={true} alt={'brand 1 logo'} />
+                    </div>
+                    <div>
+                        <Image src="/logo.png" fill={true} alt={'brand 1 logo'} />
+                    </div>
+                    <div>
+                        <Image src="/logo.png" fill={true} alt={'brand 1 logo'} />
+                    </div>
+                    <div>
+                        <Image src="/logo.png" fill={true} alt={'brand 1 logo'} />
+                    </div>
+                </div>
+            </Container>
+        </Section>
     )
 }
 
