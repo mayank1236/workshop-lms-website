8485eb7ca3ba8ac538f5d27c1e64d9b5a092770c
Mayank Gupta <mayankgupta14042000@gmail.com>
2023-05-23 00:31:49 +0530

    signup form added

---
+++ src/components/sections/newsletter.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Section from '../Layout/Section'
+import Container from '../Layout/Container'
+
+const newsletter = () => {
+    return (
+        <Section>
+            <Container>
+                <></>
+            </Container>
+        </Section>
+    )
+}
+
+export default newsletter
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useEffect } from 'react';
 
 export default function Document() {
+
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script src="https://chimpstatic.com/mcjs-connected/js/users/9d8dcd663984aed5841bb2b05/f9c324ff7c499e1db876c9a5a.js" async={true}></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
