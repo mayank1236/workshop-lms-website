0afb22adabd3aa579dc1938794939c6113d388ab
Mayank Gupta <mayankgupta14042000@gmail.com>
2023-06-03 20:32:23 +0530

    price notice

--- src/components/user/PaymentForm.tsx
+++ src/components/user/PaymentForm.tsx
@@ -13,7 +13,7 @@ const PaymentForm = () => {
     const [payment, setPaymentForm] = useState(null);
     const [transactionid, setTransactionid] = useState("");
     const [email, setEmail] = useState("");
-    
+
 
 
 
@@ -23,22 +23,21 @@ const PaymentForm = () => {
 
 
     const handlePayment = async () => {
-        console.log(name,email)
-        if(transactionid!='' && email!='')
-        {
+        console.log(name, email)
+        if (transactionid != '' && email != '') {
             let obj = {
-                transactionid:transactionid,
-                email:email,
-               
+                transactionid: transactionid,
+                email: email,
+
             }
             console.log(obj)
             const config = {
-            headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+                }
             }
-            }
-            const i = await axios.post('http://13.127.222.180:3001/v1/user/transaction',obj, config)
+            const i = await axios.post('http://13.127.222.180:3001/v1/user/transaction', obj, config)
             console.log(i)
             // if(i.data.status)
             // {
@@ -51,8 +50,7 @@ const PaymentForm = () => {
             alert('Payment successfully');
             Router.push('/')
         }
-        else
-        {
+        else {
             alert('Please Enter Fields')
         }
         // const config = {
@@ -72,24 +70,27 @@ const PaymentForm = () => {
             <Container>
                 <h2>Pay Now</h2>
                 <div className={style.form} style={{ maxWidth: "500px", margin: "0 auto" }}>
-                <Image src={'/mandalaconfluence/qr.jpeg'} alt={'Mandala Banner'} width= '300' height='100'style={{ position:"relative",height: "300px", width: "100%", objectFit: "contain" }} />
-                <label>
+                    <Image src={'/mandalaconfluence/qr.jpeg'} alt={'Mandala Banner'} width='300' height='100' style={{ position: "relative", height: "300px", width: "100%", objectFit: "contain" }} />
+                    <label>
+                        The price reflected here - Rs. 5999 ($74) - will only be valid until midnight tonight. After that, it should be Rs. 6499 ($79).
+                    </label>
+                    <label>
                         Transaction Id *
                         <input type="text" value={transactionid} onChange={(val) => {
-                                setTransactionid(val.target.value);
-                                
-                            }}/>
-                        </label>
-                        <label>
-                            Registered Email *
-                            <input type="email" value={email} onChange={(val) => {
-                                  setEmail(val.target.value);
-                                  
-                                }}/>
-                        </label>
-                        <button style={{ width: "100%" }} type="button" onClick={handlePayment}>
-                            Submit
-                        </button>
+                            setTransactionid(val.target.value);
+
+                        }} />
+                    </label>
+                    <label>
+                        Registered Email *
+                        <input type="email" value={email} onChange={(val) => {
+                            setEmail(val.target.value);
+
+                        }} />
+                    </label>
+                    <button style={{ width: "100%" }} type="button" onClick={handlePayment}>
+                        Submit
+                    </button>
                 </div>
             </Container>
         </Section>
